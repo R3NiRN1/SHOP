0255fb31ed9a069ccc5fde05b37fe1291ebaddde
REN <r3nirn1@gmail.com>
2026-08-31 00:37:10 +0100

    fix: override Next JavaScript parser under ESLint 10

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,3 +1,19 @@
+import tsParser from '@typescript-eslint/parser';
 import nextConfig from 'eslint-config-next/core-web-vitals';
 
-export default nextConfig;
+export default [
+  ...nextConfig,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: {
+      react: { version: '19.2' },
+    },
+  },
+];
